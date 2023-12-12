142333205e7923aaaa1828ac4b77b9e04596ccf5
Michael Matiushyn <30658950+MichaelMatiushyn@users.noreply.github.com>
2023-12-12 16:42:51 +0200

    20231212 added from w

--- js/index.js
+++ js/index.js
@@ -3,5 +3,8 @@ console.log("Денчик Супермен");
 
 const shouldConfirm = confirm('Хочете продовжити підписку');
 console.log(shouldConfirm);
+
+const enterText = prompt('Enter your password');
+console.log(enterText);
 // 
 
